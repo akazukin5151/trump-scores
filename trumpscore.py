b285e1039104cbe3000dc80a7884ad900857863b
twenty5151 <tsuiyikching@protonmail.com>
2020-06-14 10:59:17 +0800

    Remove sep arg to improve colormap

--- trumpscore.py
+++ trumpscore.py
@@ -9,7 +9,7 @@ df2.district = df2.district.astype(str)
 df2 = df2.sort_values('plus_minus')
 df2 = df2.dropna()
 
-cm = sns.diverging_palette(220, 10, sep=80, as_cmap=True)
+cm = sns.diverging_palette(220, 10, as_cmap=True)
 s = (df2.style
         .background_gradient(cmap=cm)
         .set_precision(3)
